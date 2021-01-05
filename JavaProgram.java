f68ce28fb6d16b06785b03b3e367046b1196907a
akarshg154 <akarshg154@gmail.com>
2021-01-05 21:56:21 +0530

    added 1 line

--- JavaProgram.java
+++ JavaProgram.java
@@ -9,6 +9,8 @@ class Main {
    System.out.println("First Element: " + age[0]);
    System.out.println("Second Element: " + age[1]);
    System.out.println("Third Element: " + age[2]);
+   System.out.println("Third Element: " + age[2]);
+   
    System.out.println("Fourth Element: " + age[3]);
    System.out.println("Fifth Element: " + age[4]);
  }
